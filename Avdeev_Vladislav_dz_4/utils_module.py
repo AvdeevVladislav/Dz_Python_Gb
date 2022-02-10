2246e823cbd4ed85ad46d7deb00ce7335be7654d
AvdeevVladislav <97964735+AvdeevVladislav@users.noreply.github.com>
2022-02-10 20:00:25 +0300

    Дз 5

---
+++ Avdeev_Vladislav_dz_4/utils_module.py
@@ -0,0 +1,14 @@
+import requests
+from decimal import *
+
+getcontext().prec = 4
+
+def currency_rates(money):
+    money = money.upper()
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
+
+    if money not in response:
+        return None
+
+    rub = response[response.find('<Value>', response.find(money)) + 7:response.find('</Value', response.find(money))]
+    return f"{Decimal(rub.replace(',', '.'))}"
